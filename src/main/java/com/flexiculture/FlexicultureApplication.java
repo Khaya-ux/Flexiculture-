b4695176af355c4451d3dfd4727a4b36ba0e9b9a
ThabangLenonyana <lenonyanathabang@gmail.com>
2025-03-08 23:05:15 +0200

    Initialize Spring Boot project structure and configuration

---
+++ src/main/java/com/flexiculture/FlexicultureApplication.java
@@ -0,0 +1,13 @@
+package com.flexiculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexicultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexicultureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flexiculture/FlexicultureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexiculture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexicultureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
